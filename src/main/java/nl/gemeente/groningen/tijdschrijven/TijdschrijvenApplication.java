cae861fd125feb1b2ff448f244b7599ba10ef116
Maarten Millaard <Maarten.Millaard@planet.nl>
2019-02-01 16:10:26 +0100

    opgeschoond en html-template t.b.v. thymeleaf toegevoegd

--- src/main/java/nl/gemeente/groningen/tijdschrijven/TijdschrijvenApplication.java
+++ src/main/java/nl/gemeente/groningen/tijdschrijven/TijdschrijvenApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TijdschrijvenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TijdschrijvenApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(TijdschrijvenApplication.class, args);
+    }
 
 }
-
