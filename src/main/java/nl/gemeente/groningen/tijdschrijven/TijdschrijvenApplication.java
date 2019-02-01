34310a7c786ca35addadd9e258d7a589a91f4de9
Maarten Millaard <Maarten.Millaard@planet.nl>
2019-02-01 13:54:19 +0100

    initieel

---
+++ src/main/java/nl/gemeente/groningen/tijdschrijven/TijdschrijvenApplication.java
@@ -0,0 +1,14 @@
+package nl.gemeente.groningen.tijdschrijven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TijdschrijvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TijdschrijvenApplication.class, args);
+	}
+
+}
+
