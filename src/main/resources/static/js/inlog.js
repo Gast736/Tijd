fc8124a33bca05bb2148f198887aa6cbcf58b393
Arjan Hegge <ahegge@home.nl>
2019-02-08 10:19:19 +0100

    Javacript concept voor submit inlog toegevoegd

--- src/main/resources/static/js/inlog.js
+++ src/main/resources/static/js/inlog.js
@@ -10,19 +10,20 @@ Dit javasscript zorgt voor de functionaliteit op de inlogpagina.
 
 
 function toonMedewerkers(jsonrecordset) {
-/* Deze functie toont alle medewerkers:
-- doorloopt het json object
-- pakt daaruit de namen
-- en plaatst deze in het div-element ophalenMdw.*/    
+    /* Deze functie toont alle medewerkers:
+    - doorloopt het json object
+    - pakt daaruit de namen
+    - en plaatst deze in het div-element ophalenMdw.*/
     var s = "";
     // LOGGING AAN
-    console.log("Het JSON object met " + jsonrecordset.length + "records, is ontvangen.");
+    console.log("Het JSON object met " + jsonrecordset.length + " records, is ontvangen.");
+
     for (var i = 0; i < jsonrecordset.length; i++) {
         s = s + "<option>" + jsonrecordset[i].naam + "</option>";
         // LOGGING AAN
         console.log("Medewerker: " + jsonrecordset[i].naam + " toegevoegd.");
     }
-    s='<select id="inputName" class="form-control" placeholder="Naam medewerker" required autofocus>' + s + "</select>";
+    s = '<select id="inputName" class="form-control" placeholder="Naam medewerker" required autofocus>' + s + "</select>";
     document.getElementById("ophalenMdw").innerHTML = s;
     return false;
 }
@@ -30,9 +31,9 @@ function toonMedewerkers(jsonrecordset) {
 
 
 function getData(surl, callback) {
-/*
-Deze functie begeleidt de aanroep van de restcontroller. Er moet nog wel wat aan exception handling worden toegevoegd.
-*/
+    /*
+    Deze functie begeleidt de aanroep van de restcontroller. Er moet nog wel wat aan exception handling worden toegevoegd.
+    */
     $.ajax({
         url: surl,
 
@@ -50,9 +51,33 @@ Deze functie begeleidt de aanroep van de restcontroller. Er moet nog wel wat aan
 
 
 // De code die bij het gereed zijn van de pagina wordt uitgevoerd. Maakt gebruik van de twee hierboven gedefinieerde functies
-    $(document).ready(function(){
+$(document).ready(function () {
 
-    getData("http://localhost:8080/medewerkers", toonMedewerkers);
+    getData("http://localhost:8080/medewerkers/all", toonMedewerkers);
 
-    console.log("document is ready");
+    console.log("Het document is geladen. (document.ready)");
+});
+
+/*
+Checken of het ingevoerde wachtwoord overeenkomt. We sturen de combinatie gebruiker en wachtwoord naar de controller en verwachten
+als resultaat een TRUE or FALSE terug.
+*/
+
+$('#submitBtn').click(function () {
+    // LOGGING AAN
+    console.log("We sturen het volgende wachtwoord naar de controller: " + $('#inputPassword').val());
+    console.log("We sturen de volgende medewerker naar de controller: " + $('#inputName').val());
+
+    $.ajax('/medewerker/wachtwoord', {
+        data: {
+            medewerker: $('#inputName').val(),
+            wachtwoord: $('#inputPassword').val()
+
+        }
+    }).done(function (data) {
+        // LOGGING AAN
+        console.log("We krijgen het volgende resultaat terug van de controller: " + data)
+        $('#submitResult') = text(data);
+        // Bij TRUE moeten we hier nog toevoegen dat je verder gaat naar registratie.html
+    });
 });
