60e01216bc7540f8ba69456891887b1fc8c6813b
Maarten Millaard <Maarten.Millaard@planet.nl>
2019-02-22 15:01:22 +0100

    medewerkers.html + js verwijderd; medewerkers2.html + js hernoemd.

--- src/main/resources/static/js/medewerkers.js
+++ src/main/resources/static/js/medewerkers.js
@@ -1,102 +1,130 @@
+var medewerkers = [];
+
+function vulMedewerkers(m) {
+	medewerkers.push(m);
+}
+
 function createSelectRole(idmedewerker, rol) {
 	let options = ["Beheerder", "Afdelingshoofd", "Medewerker"];
-	let select = `<td><select class='form-control' id='rollen-idmedewerker'>`;
-	for (i=0; i<options.length;i++) {
+	let select = `<td><select class='form-control' id='rol-` + idmedewerker +`'>`;
+	for (let i = 0; i < options.length; i++) {
 		let s = "";
 		if (rol.toUpperCase() == options[i].toUpperCase()) {
 			s = `selected`;
-			}
+		}
 		select = select + `<option ` + s + `>` + options[i] + `</option>`;
-		
+
 	}
 	select = select + `</select></td>`;
 	return select;
 }
 
-function readMedewerkers() {
-		$.ajax('/medewerkers').done(function(data) {
-			$.each(data, function(idx, el) {
-				let h = `
-				<tr class="medewerker">
-					<td>${el.idmedewerker}</td>
-					<td><input class="form-control" id="emailadres-${el.idmedewerker}" type="text" required maxlength="45" value="${el.emailadres}"></td>
-					<td><input class="form-control" id="naam-${el.idmedewerker}" type="text" required maxlength="45" value="${el.naam}"></td>
-					<td><input class="form-control" id="wachtwoord-${el.idmedewerker}" type="text" required maxlength="45" value="${el.wachtwoord}"></td>
-					<td><input class="form-control" id="team-${el.idmedewerker}" type="text" required maxlength="45" value="${el.team}"></td>` + createSelectRole(el.idmedewerker, el.rol) + `
-					<td><input class="form-control" id="contracturen-${el.idmedewerker}" type="text" required maxlength="45" value="${el.contracturen}"></td>
-					<td><input class="form-control" id="startdatum-${el.idmedewerker}" type="date" required maxlength="45" value="${el.startdatum}"></td>
-					<td><input class="form-control" id="einddatum-${el.idmedewerker}" type="date" required maxlength="45" value="${el.einddatum}"></td>
-					<td><button type="button" class="btn btn-primary btnOpslaan" id="${el.idmedewerker}">Opslaan</button></td>
-				</tr>
+function haalMedewerkers() {
+	$.ajax("/medewerkers").done(function(data) {
+		$.each(data, function(idx, elem) {
+			let medewerker = {
+					idmedewerker : elem.idmedewerker,
+					emailadres : elem.emailadres,
+					naam : elem.naam,
+					wachtwoord : elem.wachtwoord,
+					team: elem.team,
+					rol: elem.rol,
+					contracturen : elem.contracturen,
+					startdatum : elem.startdatum,
+					einddatum : elem.einddatum
+			};
+			let row = `
+				<div class="row"  id='rij-` + medewerker.idmedewerker +`'>
+				<div  id='medewerker-` + medewerker.idmedewerker +`'>` + medewerker.idmedewerker + `</div>
+				<div class="col-2">
+				<input class="form-control" type="text" required maxlength="45"
+				value="` + medewerker.emailadres + `" id='emailadres-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-2">
+				<input class="form-control" type="text" required maxlength="45"
+				value="` + medewerker.naam + `" id='naam-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-2">
+				<input class="form-control" type="text" required maxlength="45"
+				value="` + medewerker.wachtwoord + `" id='wachtwoord-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-1">
+				<input class="form-control" type="text" required maxlength="45"
+				value="` + medewerker.team + `" id='team-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-1">` + createSelectRole(medewerker.idmedewerker, medewerker.rol) + `
+				</div>
+				<div class="col-1">
+				<input class="form-control" type="number" required maxlength="45"
+				value="` + medewerker.contracturen + `" id='contracturen-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-1">
+				<input class="form-control" type="date" required maxlength="45"
+				value="` + medewerker.startdatum + `" id='startdatum-` + medewerker.idmedewerker +`'>
+				</div>
+				<div class="col-1">
+				<input class="form-control" type="date" required maxlength="45"
+				value="` + medewerker.einddatum + `" id='einddatum-` + medewerker.idmedewerker +`'>
+				</div>
+				<div><button type="button" class="btn btn-primary btnOpslaan" id='` + medewerker.idmedewerker + `'>Opslaan</button></div>
+				</div>
 				`;
-				$("#mdwTbody").html($("#mdwTbody").html() + h);
-			})
-		}).fail(function(e) {
-			console.log(e)
-		})
-	};
+			$("#mdwTbody").html($("#mdwTbody").html() + row);
 
-function insertMedewerker() {
-    $.ajax('/newMedewerker', {
-        data: {
-            emailadres: $('#emailadres').val(),
-            naam: $('#naam').val(),
-            wachtwoord: $('#wachtwoord').val(),
-            team: $('#team').val(),
-            rol: $('#rol').val(),
-            contracturen: $('#contracturen').val(),
-            startdatum: $('#startdatum').val(),
-            einddatum: $('#einddatum').val()
-        },
-        method: 'POST'
-    }).done(function(){
-    	$("#mdwTbody").html($(""));
-    }).done(function(){
-    	readMedewerkers();
-    })
-};
+		})
+	});
+}
+
+function nieuweMedewerker() {
+	$.ajax('/newMedewerker', {
+		data: {
+			emailadres: $('#emailadres').val(),
+			naam: $('#naam').val(),
+			wachtwoord: $('#wachtwoord').val(),
+			team: $('#team').val(),
+			rol: $('#rol').val(),
+			contracturen: $('#contracturen').val(),
+			startdatum: $('#startdatum').val(),
+			einddatum: $('#einddatum').val()
+		},
+		method: 'POST'
+	}).done(function(){
+		location.reload();
+	})
+}
 
 $('#btnNewMdw').click(function(){
-		insertMedewerker();
+	nieuweMedewerker();
 });
 
-$('#mdwTbody').on('click', '.btnOpslaan', function(el){
-	let idmedewerker = el.currentTarget.id;
-	let email = $('#emailadres-' + idmedewerker).val();
-	let naam = $('#naam-' + idmedewerker).val(); 
-	let wachtwoord = $('#wachtwoord-' + idmedewerker).val();
-	let team = $('#team-' + idmedewerker).val();
-	let rol = $('select').val(); 
-	let contracturen = $('#contracturen-' + idmedewerker).val(); 
-	let startdatum = $('#startdatum-' + idmedewerker).val(); 
-	let einddatum = $('#einddatum-' + idmedewerker).val();
-	
+$('#mdwTbody').on('click', '.btnOpslaan', function(e){
+	let idmedewerker = e.target.id;
+	let emailadres = $("#emailadres-"+idmedewerker).val();
+	let naam = $("#naam-"+idmedewerker).val();
+	let wachtwoord = $("#wachtwoord-"+idmedewerker).val();
+	let team = $("#team-"+idmedewerker).val();
+	let rol = $("#rol-"+idmedewerker).val();
+	let contracturen = $("#contracturen-"+idmedewerker).val();
+	let startdatum = $("#startdatum-"+idmedewerker).val();
+	let einddatum = $("#einddatum-"+idmedewerker).val();
 	$.ajax('/changeMedewerker', {
 		data: {
-			idmedewerker: idmedewerker, 
-			emailadres: email, 
-			naam: naam, 
+			idmedewerker: idmedewerker,
+			emailadres: emailadres,
+			naam: naam,
 			wachtwoord: wachtwoord,
-		    team: team, 
-		    rol: rol, 
-		    contracturen: contracturen, 
-		    startdatum: startdatum, 
-		    einddatum: einddatum
+			team: team,
+			rol: rol,
+			contracturen: contracturen,
+			startdatum: startdatum,
+			einddatum: einddatum
 		},
 		method: 'PUT'
 	}).done(function(){
-		console.log("medewerker " + idmedewerker + " is opgeslagen...");
+		console.log("Medewerker" + idmedewerker + "is opgeslagen");
 	})
-});
-
-$('#mdwTbody').on('change', 'select', function(el){
-	let targetId = el.currentTarget.id;
-	let id = targetId.substr(targetId.indexOf("-"))
-	console.log(id);
-	createSelectRole(id, $(this).val());
 })
 
-$(document).ready(function(){
-	readMedewerkers();
+$("document").ready(function() {
+	haalMedewerkers();
 });
-
