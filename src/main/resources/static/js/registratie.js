07aea5500a732268d04c7fd7153f2c6e3f77cc06
Arjan Hegge <ahegge@home.nl>
2019-02-23 21:52:28 +0100

    Beheermenu alleen beschikbaar voor beheerder
    Ingericht d.m.v. cookie met de rol. Alleen gebruikers met de rol
    'beheerder' (Arjan), ziet nu dit menu.

--- src/main/resources/static/js/registratie.js
+++ src/main/resources/static/js/registratie.js
@@ -86,6 +86,23 @@ function berekenTotalen() { // eerst de rijtotalen
 }
 
 /*
+Deze functie zorgt voor toevoeging van het beheerdersmenu als de gebruiker de juiste rol heeft
+*/
+function addBeheerMenu() {
+    if (rol=="beheerder") {
+        $('#navbarList').append(`<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> Beheer</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown02">
+                        <a class="dropdown-item" href="/projecten.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder-plus"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path><line x1="12" y1="11" x2="12" y2="17"></line><line x1="9" y1="14" x2="15" y2="14"></line></svg> Projecten</a>
+                        <a class="dropdown-item" href="/medewerkers.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg> Medewerkers</a>
+                    </div>
+                </li>`);
+    } else {
+        console.log("Op grond van de rol: " + rol + " wordt het beheermenu niet getoond.")
+    }
+}
+
+/*
 Deze functie vult de weken 1 t/m 53 voor het selectieveld op het registratieformulier
 */
 function vulWeken() {
@@ -320,11 +337,21 @@ function bouwFormulierOp() {
                 </div>
             </div>
             <br>
-            <div class="row justify-content-between">
+            <div class="row">
                 <div class="col-sm-3">
                     <strong>Nog te schrijven</strong>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
                     <input type="number" class="form-control restant" id="resterend" placeholder="0">
                 </div>
              </div>   
@@ -360,10 +387,11 @@ $('.periodSelect').change(function () {
 Event bij de eerste keer laden van de pagina
 */
 
-$(document).ready(function () {
+$(document).ready(function () {;
     console.log("pagina opnieuw geladen (document.ready)");
     checkCookie();
     vulWeken();
+    addBeheerMenu();
     haalDatums();
     haalProjecten();
     setTimeout(bouwFormulierOp, 500);
