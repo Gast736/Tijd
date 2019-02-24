391652be30aee33f11d87b7c542bbb53b3e7efd5
Arjan Hegge <ahegge@home.nl>
2019-02-24 14:44:48 +0100

    Persoonlijk.js Eerste aanzet gemaakt met chart.js
    Nu nog gevuld op basis van een 'harde' gegevensset. Ik schat in dat ik
    de input hiervoor in Javascript wel vrij snel kan opbouwen door door een
    JSON object (vanuit de controller) heen te lo(o)pen. Deze kleurstelling
    is trouwens prut.... Hebben jullie daar nog ideeën over? Ook hier kunnen
    we een array samenstellen van kleurcodes waar doorheen geïtereerd kan
    worden.

--- src/main/resources/static/js/persoonlijk.js
+++ src/main/resources/static/js/persoonlijk.js
@@ -56,7 +56,7 @@ function getCookie(cname) {
 Deze functie zorgt voor toevoeging van het beheerdersmenu als de gebruiker de juiste rol heeft
 */
 function addBeheerMenu() {
-    if (rol=="beheerder") {
+    if (rol == "beheerder") {
         $('#navbarList').append(`<li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> Beheer</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown02">
@@ -94,6 +94,91 @@ function haalUrenPerProjectPerMaand() {
     });
     return false;
 }
+
+var barChartData = {
+    labels: ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
+    datasets: [{
+        label: 'Verlof',
+        backgroundColor: '#d9e6f2',
+        data: [
+					0,
+					40,
+					0,
+					0,
+					0,
+					40,
+					0,
+					120,
+                    0,
+					0,
+					0,
+                    40
+				]
+
+            }, {
+        label: 'Financiële Managementinfo',
+        backgroundColor: '#4080bf',
+        data: [
+					40,
+					0,
+					40,
+					20,
+					20,
+					40,
+					40,
+					0,
+                    40,
+					40,
+					60,
+                    40
+				]
+			}, {
+        label: 'DMO',
+        backgroundColor: '#264d73',
+        data: [
+					80,
+					80,
+					80,
+					100,
+					100,
+					40,
+					80,
+					0,
+                    80,
+					80,
+					60,
+                    40
+				]
+			}]
+
+};
+
+window.onload = function () {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+						}],
+                yAxes: [{
+                    stacked: true
+						}]
+            }
+        }
+    });
+};
 
 
 /* 
