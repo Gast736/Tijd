9c2bb898b380ad168a3a033d9fee656499f2a68e
Maarten Millaard <Maarten.Millaard@planet.nl>
2019-02-28 23:12:17 +0100

    Laatste wijzigingen: Nieuwe medewerkers en projecten opvoeren/wijzigen; Tabellen aanvullen met nieuwe rij; Beheerdermenu toegevoegd aan project- en medewerkerspagina; tekst met ingelogde gebruiker naast Uitlogknop geplaatst

--- src/main/resources/static/js/medewerkers.js
+++ src/main/resources/static/js/medewerkers.js
@@ -1,16 +1,18 @@
-var medewerkers = [];
+var medewerkerid;
+var contracturen;
+var rol;
 
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+	var c = ca[i];
+	while (c.charAt(0) == ' ') {
+	    c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+	    return c.substring(name.length, c.length);
+	}
     }
     return "";
 }
@@ -30,10 +32,27 @@ function checkCookie() {
     }
 }
 
-function vulMedewerkers(m) {
-    medewerkers.push(m);
+/*
+Deze functie zorgt voor toevoeging van het beheerdersmenu als de gebruiker de juiste rol heeft
+ */
+function addBeheerMenu() {
+    if (rol.toLowerCase() == "beheerder") {
+	$('#navbarList').append(`<li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> Beheer</a>
+		<div class="dropdown-menu" aria-labelledby="dropdown02">
+		<a class="dropdown-item" href="/projecten.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder-plus"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path><line x1="12" y1="11" x2="12" y2="17"></line><line x1="9" y1="14" x2="15" y2="14"></line></svg> Projecten</a>
+		<a class="dropdown-item" href="/medewerkers.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg> Medewerkers</a>
+		</div>
+	</li>`);
+    } else {
+	console.log("Op grond van de rol: " + rol + " wordt het beheermenu niet getoond.")
+    }
 }
 
+/*
+ *  Functie om de rol van de medewerker in een listbox te zetten
+ *  De huidige rol wordt het actieve item in de lijst
+ */
 function createSelectRole(idmedewerker, rol) {
     let options = ["Beheerder", "Afdelingshoofd", "Medewerker"];
     let select = `<td><select class='form-control' id='rol-` + idmedewerker +`'>`;
@@ -69,11 +88,11 @@ function haalMedewerkers() {
 		<input class="form-control" type="text" required maxlength="45"
 		value="` + medewerker.emailadres + `" id='emailadres-` + medewerker.idmedewerker +`'>
 		</div>
-		<div class="col-2">
+		<div class="col-1">
 		<input class="form-control" type="text" required maxlength="45"
 		value="` + medewerker.naam + `" id='naam-` + medewerker.idmedewerker +`'>
 		</div>
-		<div class="col-2">
+		<div class="col-1">
 		<input class="form-control" type="text" required maxlength="45"
 		value="` + medewerker.wachtwoord + `" id='wachtwoord-` + medewerker.idmedewerker +`'>
 		</div>
@@ -87,11 +106,11 @@ function haalMedewerkers() {
 		<input class="form-control" type="number" required maxlength="45"
 		value="` + medewerker.contracturen + `" id='contracturen-` + medewerker.idmedewerker +`'>
 		</div>
-		<div class="col-1">
+		<div class="col-2">
 		<input class="form-control" type="date" required maxlength="45"
 		value="` + medewerker.startdatum + `" id='startdatum-` + medewerker.idmedewerker +`'>
 		</div>
-		<div class="col-1">
+		<div class="col-2">
 		<input class="form-control" type="date" required maxlength="45"
 		value="` + medewerker.einddatum + `" id='einddatum-` + medewerker.idmedewerker +`'>
 		</div>
@@ -117,7 +136,56 @@ function nieuweMedewerker() {
 	    einddatum: $('#einddatum').val()
 	},
 	method: 'POST'
-    }).done(getResult(res))
+    }).done(function (res) {
+	console.log('res', res);
+	if (!res) {
+	    $('#submitResult').removeClass("alert alert-success");
+	    $('#submitResult').addClass("alert alert-danger");
+	    $('#submitResult').attr("role", "alert");
+	    $('#submitResult').text("De data kon niet worden verzonden.");
+	} else {
+	    let row = `
+		<div class="row"  id='rij-` + res +`'>
+		<div class="col-2">
+		<input class="form-control" type="text" required maxlength="45"
+		value="` + $('#emailadres').val() + `" id='emailadres-` + res +`'>
+		</div>
+		<div class="col-1">
+		<input class="form-control" type="text" required maxlength="45"
+		value="` + $('#naam').val() + `" id='naam-` + res +`'>
+		</div>
+		<div class="col-1">
+		<input class="form-control" type="text" required maxlength="45"
+		value="` + $('#wachtwoord').val() + `" id='wachtwoord-` + res +`'>
+		</div>
+		<div class="col-1">
+		<input class="form-control" type="text" required maxlength="45"
+		value="` + $('#team').val() + `" id='team-` + res +`'>
+		</div>
+		<div class="col-1">` + createSelectRole(res, $('#rol').val()) + `
+		</div>
+		<div class="col-1">
+		<input class="form-control" type="number" required maxlength="45"
+		value="` + $('#contracturen').val() + `" id='contracturen-` + res +`'>
+		</div>
+		<div class="col-2">
+		<input class="form-control" type="date" required maxlength="45"
+		value="` + $('#startdatum').val() + `" id='startdatum-` + res +`'>
+		</div>
+		<div class="col-2">
+		<input class="form-control" type="date" required maxlength="45"
+		value="` + $('#einddatum').val() + `" id='einddatum-` + res +`'>
+		</div>
+		<div class="col"><button type="button" class="btn btn-primary btnOpslaan" id='` + res + `'>Opslaan</button></div>
+		</div>
+		`;
+	    $('#mdwTbody').append(row);
+	    $('#submitResult').removeClass("alert alert-danger");
+	    $('#submitResult').addClass("alert alert-success");
+	    $('#submitResult').attr("role", "alert");
+	    $('#submitResult').text("De ingevulde data is opgeslagen");
+	}
+    })
 }
 
 $('#btnNewMdw').click(function(){
@@ -147,25 +215,24 @@ $('#mdwTbody').on('click', '.btnOpslaan', function(e){
 	    einddatum: einddatum
 	},
 	method: 'PUT'
-    }).done(getResult(res))
+    }).done(function (res) {
+	console.log('res', res);
+	if (!res) {
+	    $('#submitResult').removeClass("alert alert-success");
+	    $('#submitResult').addClass("alert alert-danger");
+	    $('#submitResult').attr("role", "alert");
+	    $('#submitResult').text("De data kon niet worden verzonden.");
+	} else {
+	    $('#submitResult').removeClass("alert alert-danger");
+	    $('#submitResult').addClass("alert alert-success");
+	    $('#submitResult').attr("role", "alert");
+	    $('#submitResult').text("De ingevulde data is opgeslagen");
+	}
+    })
 })
-
-function getResult(res) {
-    console.log('res', res);
-    if (!res) {
-	$('#submitResult').addClass("alert alert-danger");
-	$('#submitResult').attr("role", "alert");
-	$('#submitResult').text("De gegevens konden niet worden verzonden.");
-    } else {
-	location.reload();
-	$('#submitResult').addClass("alert alert-success");
-	$('#submitResult').attr("role", "alert");
-	$('#submitResult').text("De gegevens zijn opgeslagen");
-	return res;
-    }
-}
 
 $("document").ready(function() {
     checkCookie();
+    addBeheerMenu();
     haalMedewerkers();
 });
--- src/main/resources/static/js/projecten.js
+++ src/main/resources/static/js/projecten.js
@@ -1,3 +1,7 @@
+var medewerkerid;
+var contracturen;
+var rol;
+
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -17,9 +21,9 @@ function checkCookie() {
     var user = getCookie("user");
 
     if (user != "") {
-        let medewerkerid = getCookie("id");
-        let rol = getCookie("rol");
-        let contracturen = getCookie("contracturen");
+        medewerkerid = getCookie("id");
+        rol = getCookie("rol");
+        contracturen = getCookie("contracturen");
         console.log("De id van ingelogde medewerker is: " + medewerkerid);
         $('#employeeName').text(user);
     } else {
@@ -28,6 +32,27 @@ function checkCookie() {
     }
 }
 
+/*
+Deze functie zorgt voor toevoeging van het beheerdersmenu als de gebruiker de juiste rol heeft
+*/
+function addBeheerMenu() {
+    if (rol.toLowerCase() == "beheerder") {
+        $('#navbarList').append(`<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg> Beheer</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown02">
+                        <a class="dropdown-item" href="/projecten.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder-plus"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path><line x1="12" y1="11" x2="12" y2="17"></line><line x1="9" y1="14" x2="15" y2="14"></line></svg> Projecten</a>
+                        <a class="dropdown-item" href="/medewerkers.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg> Medewerkers</a>
+                    </div>
+                </li>`);
+    } else {
+        console.log("Op grond van de rol: " + rol + " wordt het beheermenu niet getoond.")
+    }
+}
+
+/*
+ * Functie om de categorie van een project in een listbox te zetten.
+ * De huidige categorie wordt het actieve item
+ */
 function createSelectCategorie(idproject, categorie) {
     let options = ["Project", "Intern project", "Afwezigheid"];
     let select = `<td><select class='form-control' id='categorie-` + idproject +`'>`;
@@ -93,6 +118,18 @@ function nieuwProject() {
 	    $('#submitResult').addClass("alert alert-success");
 	    $('#submitResult').attr("role", "alert");
 	    $('#submitResult').text("De gegevens zijn opgeslagen");
+	    let row = `
+		<div class="row"  id='rij-` + res +`'>
+		<div class="col-2">` + createSelectCategorie(res, $('#categorie').val()) + `</div>
+		<div class="col-2"><input class="form-control" id="naam-` + res + `" type="text" required maxlength="45" value="` + $('#naam').val() + `"></div>
+		<div class="col-2"><input class="form-control" id="opdrachtgever-` + res + `" type="text" required maxlength="45" value="` + $('#opdrachtgever').val() + `"></div>
+		<div class="col-1"><input class="form-control" id="directie-` + res + `" type="text" required maxlength="45" value="` + $('#directie').val() + `"></div>
+		<div class="col-2"><input class="form-control" id="startdatum-` + res + `" type="date" required value="` + $('#startdatum').val() + `"></div>
+		<div class="col-2"><input class="form-control" id="einddatum-` + res + `" type="date" value="` + $('#einddatum').val() + `"></div>
+		<div class="col"><button type="button" class="btn btn-primary btnOpslaan" id='` + res + `'>Opslaan</button></div>
+		</div>
+		`;
+	    $('#projTbody').append(row);
 	}
     })
 }
@@ -136,5 +173,6 @@ $('#projTbody').on('click', '.btnOpslaan', function(e){
 
 $("document").ready(function() {
     checkCookie();
+    addBeheerMenu();
     haalProjecten();
 });
